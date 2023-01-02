8fd96ea85dab26b13582529983c530853b651bfc
Logan Waite <lwaite2010@gmail.com>
2023-01-02 16:44:23 -0500

    finnish basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,39 @@
 	import ScoreRow from '$lib/components/score-row.svelte';
 </script>
 
-<h1 class="title">Qwix!</h1>
-<div class="dice-tray">
-	<Die value={1} />
-	<Die value={2} />
-	<Die value={3} color="red" pipColor="white" />
-	<Die value={4} color="yellow" />
-	<Die value={5} color="green" pipColor="white" />
-	<Die value={6} color="blue" pipColor="white" />
-</div>
-<!-- <div class="actions">
-    <button>Roll</button>
-</div> -->
-<div class="scorecard">
-	<ScoreRow color="red" ascOrder={true} />
-	<ScoreRow color="yellow" ascOrder={true} />
-	<ScoreRow color="green" ascOrder={false} />
-	<ScoreRow color="blue" ascOrder={false} />
-</div>
+<svelte:head>
+	<title>Qwixx Clone</title>
+</svelte:head>
+
+<h1 class="title">Qwixx!</h1>
+
+<main class="game-wrapper">
+	<div class="dice-tray">
+		<Die value={1} />
+		<Die value={2} />
+		<Die value={3} color="red" pipColor="white" />
+		<Die value={4} color="yellow" />
+		<Die value={5} color="green" pipColor="white" />
+		<Die value={6} color="blue" pipColor="white" />
+	</div>
+	<div class="actions">
+		<button class="roll-button">Roll Dice</button>
+		<div class="empty-rolls">
+			<h5>Scores Not Taken</h5>
+			<div class="empty-roll-marker__wrapper">
+				{#each Array(4).fill(0) as box}
+					<div class="empty-roll-marker" />
+				{/each}
+			</div>
+		</div>
+	</div>
+	<div class="scorecard">
+		<ScoreRow color="red" ascOrder={true} />
+		<ScoreRow color="yellow" ascOrder={true} />
+		<ScoreRow color="green" ascOrder={false} />
+		<ScoreRow color="blue" ascOrder={false} />
+	</div>
+</main>
 
 <style>
 	:global(body) {
@@ -29,6 +44,11 @@
 		align-items: center;
 		font-family: Verdana, Geneva, Tahoma, sans-serif;
 		background-color: lightgray;
+	}
+	.game-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 	.dice-tray {
 		box-sizing: border-box;
@@ -44,9 +64,44 @@
 		display: flex;
 		flex-wrap: wrap;
 	}
+	.actions {
+		padding: 15px;
+		display: flex;
+		justify-content: space-between;
+		width: 70%;
+	}
+	.roll-button {
+		height: 60px;
+		padding: 0.5em 1em;
+		font-size: 16pt;
+		border-radius: 10px;
+		box-shadow: 2px 2px 2px grey;
+		border: 1px solid gray;
+	}
+	.roll-button:active {
+		box-shadow: 0px 0px 2px grey;
+	}
+	.empty-rolls {
+		text-align: center;
+	}
+	.empty-rolls h5 {
+		margin: 0px;
+		margin-bottom: 5px;
+	}
+	.empty-roll-marker__wrapper {
+		display: flex;
+	}
+	.empty-roll-marker {
+		height: 30px;
+		width: 30px;
+		background-color: white;
+		border-radius: 10px;
+		border: 1px solid black;
+		margin: 0px 5px;
+	}
 	.scorecard {
 		/* width: 512px; */
-		padding: 10px;
-		margin-top: 10px;
+		/* padding: 10px; */
+		/* margin-top: 10px; */
 	}
 </style>
